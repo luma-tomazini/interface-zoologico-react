58d9a5b7c379a7e65d41c0094092fac3e3d719df
lumatomazini@gmail.com <lumatomazini@gmail.com>
2024-03-08 08:29:56 -0300

    Criando componente Welcome

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
-import Navegacao from './components/Navegacao/Navegacao'
-import Atracao from './components/Atracao/Atracao'
 import './App.css'
+import Home from './pages/Home/Home'
 
 function App() {
-  let componentes = []
-  for (let i = 0; i <= 2; i++) {
-    componentes.push(<Atracao key={i} />)
-  }
-
   return (
     <>
-      <Navegacao></Navegacao>
-      <div className="ctn-atracoes">
-        {componentes}
-      </div>
+      <Home />
     </>
   )
 }
--- src/components/Navegacao/Navegacao.jsx
+++ src/components/Navegacao/Navegacao.jsx
@@ -1,24 +1,23 @@
 import './Navegacao.css'
 
 function Navegacao() {
-    return(
-        <> 
-        <div className="barra-navegacao">
-        <div className="logo-zoo">
-                <h1>ZOOLOGICO</h1>
-        </div>
 
-        <div className="navbar-opcoes">
-            <ul>
-                <li>Inicio</li>
-                <li>Os Animais</li>
-                <li>Fotos</li>
-                <li>Depoimentos</li>
-                <li>Login</li>
-            </ul>
-         </div>
-         </div>
-        </>
+    return(
+        <div className="barra-navegacao">
+            <div className="logo-zoo">
+                <h1>ZOOLÓGICO</h1>
+            </div>
+
+            <div className="navbar-opcoes">
+                <ul>
+                    <li>Início</li>
+                    <li>Os Animais</li>
+                    <li>Fotos</li>
+                    <li>Depoimentos</li>
+                    <li>Login</li>
+                </ul>
+            </div>
+        </div>
     );
 }
 
