4ad155e7b34fc1cacc5443395dad3a505107ac29
lumatomazini@gmail.com <lumatomazini@gmail.com>
2024-04-19 14:39:40 -0300

    adicionando a pasta routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
 import './App.css'
-import Home from './pages/Home/Home'
-import Animais from './pages/Animais/Animais'
-import Fotos from './pages/Fotos/Fotos';
-import Depoimentos from './pages/Depoimentos/Depoimentos';
-import Login from './pages/Login/Login';
-import { BrowserRouter as Roteador, Routes, Route } from 'react-router-dom'
+import AppRoutes from './routes';
 
 function App() {
   return (
     <>
-      <Roteador>
-        <Routes>
-          <Route exact path='/' Component={Home} />
-          <Route path='/animais' Component={Animais} />
-          <Route path='/fotos' Component={Fotos} />
-          <Route path='/depoimentos' Component={Depoimentos} />
-          <Route path='/login' Component={Login} />
-
-        </Routes>
-      </Roteador>
+    <AppRoutes />
     </>
   )
 }
