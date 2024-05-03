812d160fcc21ca20b1c6111b5d8209678bac386a
lumatomazini@gmail.com <lumatomazini@gmail.com>
2024-05-03 16:45:57 -0300

    lista de cards dos animais

--- src/pages/Animais/Animais.jsx
+++ src/pages/Animais/Animais.jsx
@@ -1,14 +1,46 @@
-import Navegacao from '../../components/Navegacao/Navegacao';
-import CardAnimal from '../../components/CardAnimal/CardAnimal';
 import './Animais.css'
+import React, { useEffect, useState } from 'react';
+import CardAnimal from '../../components/CardAnimal/CardAnimal';
+import Navegacao from '../../components/Navegacao/Navegacao';
 
 function Animais() {
-    return(
+
+    // RECUPERANDO LISTA DE ANIMAIS DO SERVIDOR
+    const [animais, setAnimais] = useState(null);
+
+    // Recupera a lista de todos os animais do servidor
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/listar-aves');
+                if (!response.ok) {
+                    throw new Error('Erro ao buscar servidor');
+                }
+                const listaAnimais = await response.json();
+                setAnimais(listaAnimais);
+            } catch (error) {
+                console.error('Erro: ', error);
+            }
+        }
+
+        fetchData();
+    }, []);
+
+    return (
         <>
             <Navegacao />
-            <CardAnimal />
+            <div className='ctn-animais'>
+                {animais ? (
+                    // Renderize o seu componente para cada item da lista
+                    animais.map(animal => (
+                        <CardAnimal key={animal.idanimal} animal={animal} />
+                    ))
+                ) : (
+                    <p>Carregando...</p>
+                )}
+            </div>
         </>
     );
 }
 
-export default Animais
+export default Animais;
